49b65457a7d54916460c6c7c1e0d32991c55dd3b
cisco289 <garciacasasfrancisco@gmail.com>
2024-09-04 13:39:25 -0300

    ejercicio password.js

---
+++ 1_TUP_1_2C_2024/García_Casas_Francisco/Practica JavaS/Ejercicios/Ejercicio1.js
@@ -0,0 +1,21 @@
+/* Solicitar al usuario ingresar una contraseña.
+El programa verificara si la clave ingresada coincide con una
+clave predefinida, 
+Si la clave es incorrecta, se le permitira al usuario intentarlo nuevamente
+pero se le informara despues de tres intentos fallidos.
+*/
+var password = "123.asd";
+var contador = 0; 
+
+do{
+    var passwordin = prompt("login");
+    if (passwordin == password){
+        alert("login sucess");          //alert similar to console.log
+        break; 
+    }else{
+        alert("password incorrect");
+    }
+    contador++
+}while(contador < 3);
+
+console.log("thats not ur account")
