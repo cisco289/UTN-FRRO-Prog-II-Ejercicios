4f531999b335d92d9672879a2b5131a09ff559d9
cisco289 <garciacasasfrancisco@gmail.com>
2024-09-25 11:56:02 -0300

    /Practica_JavaS/arreglos..js

---
+++ 1_TUP_1_2C_2024/García_Casas_Francisco/Practica_JavaS/JS_Web/modulo.js
@@ -0,0 +1,40 @@
+//papa = "pelada" // *defino variable papa como un tipo de texto= 'pelada'
+//alert (papa)
+
+//? tipos de variables = var, let, const
+//const
+const num = 3.14  //* las constantes no pueden cambiar su valor (valor de IVA o de PI)
+//let
+let numero = 11
+//alert (numero)
+
+let numero2, numero3, numero4
+numero2 = 2
+numero3 = 3
+numero4 = 4
+//alert (numero2)
+
+let n = null
+//var
+//let nombre = prompt("Ingresa tu nombre")
+//alert ("hola " + nombre + " estas siendo hackeado")
+var persona = {
+    nombre: "Juan", apellido: "Garcia de la cruz", dni: 4541232,
+    nombreCompleto: function(){
+        return this.nombre + "" + this.apellido
+    }
+}
+console.log(persona) //imprimo el objeto
+
+String = "cadena de tezto"
+
+Number = 19
+
+Boolean = false // true
+
+//alert('felicidades, estas siendo hackeado') 
+
+// * sdasdasd
+// !dsasdsadsadasd 
+// ? dasdasdasd 
+// TODO: tremendo este naranja
